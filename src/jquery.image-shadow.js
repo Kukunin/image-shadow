616996fe76be88e9865a4248afcdbcbd5868d8f4
Sergiy Kukunin <sergey.kukunin@gmail.com>
2012-10-30 19:53:02 +0200

    Initial stage of Context and Multiplexer arch

--- src/jquery.image-shadow.js
+++ src/jquery.image-shadow.js
@@ -15,107 +15,126 @@
                 blur: 0,
                 blurMethod: 'native',
                 offsetX : 0,
-                offsetY : 0
+                offsetY : 0,
+                imgClass : 'shadowed',
+                wrapperClass : 'shadow-wrapper',
+                dataKey : 'image-shadow'
             }, options);
 
-            //Hardcoded values in one place
-            var v = {
-                'imgClass' : 'shadowed',
-                'wrapperClass' : 'shadow-wrapper',
-                'dataKey' : 'shadowed'
+
+            //Context for the one instance of IMG
+            //Main logic is here
+            var Context = function(img, $$) {
+                this.$$ = $$;
+                this.img = img;
+                this.$img = $(img);
+            }
+
+            Context.prototype.init = function() {
+                //If elements ins't IMG tag
+                if ( !/^img$/i.test(this.$img.prop('tagName')) ) { return; }
+                //If already shadowed
+                if ( this.isActive() ) { return; }
+
+                var doShadow = $.proxy(function() {
+                    var imageWidth = this.$img.width(),
+                        imageHeight = this.$img.height();
+
+                    var shadow = $("<canvas />")[0];
+                    if (shadow.getContext) {
+                        var context = shadow.getContext("2d");
+                            $(shadow).attr({
+                                width: imageWidth+(this.$$.blur*2),
+                                height: imageHeight+(this.$$.blur*2)
+                            });
+                            context.save();
+                            context.translate(this.$$.blur,this.$$.blur);
+                            context.shadowColor = this.$$.color;
+                            if ( this.$$.blurMethod == 'native' ) {
+                                context.shadowBlur = this.$$.blur;
+                            }
+
+                            //Make shadow separated from original image
+                            context.shadowOffsetX = imageWidth+this.$$.blur;
+                            context.shadowOffsetY = 0;
+
+                            //Draw image outside canvas, that shadow will appear on original image place
+                            context.drawImage(this.img, -imageWidth-this.$$.blur, 0, imageWidth, imageHeight);
+                            context.restore();
+
+                            if ( this.$$.blurMethod == 'manual' ) {
+                                if ( typeof window.stackBoxBlurCanvasRGBA == "function" ) {
+                                    //Dirty hack, to pass canvas into stackBoxBlurCanvasRGBA function
+                                    var origin = document.getElementById;
+                                    document.getElementById = function() { return shadow; }
+                                    stackBoxBlurCanvasRGBA( "canvas", 0, 0, imageWidth+(this.$$.blur*2), imageHeight+(this.$$.blur*2), this.$$.blur, 1 );
+                                    document.getElementById = origin;
+                                } else {
+                                    throw "StackBoxBlur.js is required for manual blurring. See http://www.quasimondo.com/BoxBlurForCanvas/FastBlur2Demo.html";
+                                }
+                            }
+                    } else {
+                        //ToDo MSIE Supporting
+                        return;
+                    }
+                    $(shadow).css({
+                        display: "block",
+                        border: 0,
+                        position: "absolute",
+                        top: this.$$.offsetY - this.$$.blur,
+                        left: this.$$.offsetX - this.$$.blur
+                    });
+
+                    this.$wrapper = $(/^a|span$/i.test(this.$img.parent().prop("tagName")) ? "<span />" : "<div />");
+                    this.wrapper = this.$wrapper[0];
+
+                    this.$wrapper.insertAfter(this.img).append([this.img, shadow])
+
+                    this.wrapper.className = this.img.className;
+                    this.$wrapper.addClass(this.$$.wrapperClass);
+
+                    this.cssText = this.wrapper.style.cssText = this.img.style.cssText;
+
+                    this.$wrapper.css({
+                        display: "block",
+                        width: imageWidth,
+                        height: imageHeight,
+                        position: "relative"
+                    });
+                    this.img.style.cssText = "display: block; border: 0px; position:relative; z-index:2; ";
+                    this.img.className = this.$$.imgClass;
+
+                    this.$img.data(this.$$.dataKey, this);
+                }, this);
+
+                if (this.img.complete)
+                    doShadow();
+                else
+                    this.$img.load(doShadow);
             };
 
-            var $shadow = {
+            Context.prototype.isActive = function() {
+                return this.$img.hasClass(this.$$.imgClass) && this.$img.parent().hasClass(this.$$.wrapperClass);
+            }
+
+            Context.prototype.unshadow = function() {
+                if ( this.isActive() ) {
+                    this.img.className = this.wrapper.className;
+                    this.img.style.cssText = this.cssText;
+                    this.$wrapper.replaceWith( this.img );
+                }
+            }
+
+            //Multiplexer, that contains the API for several elements
+            //And forward call to Context API
+            var $multiplexer = {
                 "init" : $.proxy(function() {
                     $(this).each(function() {
-                        var img = this,
-                            $img = $(img);
-                        setTimeout(function() {
-                            //If elements ins't IMG tag
-                            if ( !/^img$/i.test($img.prop('tagName')) ) { return; }
-                            //If already shadowed
-                            if ( $img.hasClass(v.imgClass) && $img.parent().hasClass(v.wrapperClass) ) {
-                                return;
-                            }
-
-                            function doShadow() {
-                                var imageWidth = $img.width(),
-                                    imageHeight = $img.height();
-
-                                var shadow = $("<canvas />")[0];
-                                if (shadow.getContext) {
-                                    var context = shadow.getContext("2d");
-                                    try {
-                                        $(shadow).attr({
-                                            width: imageWidth+(options.blur*2),
-                                            height: imageHeight+(options.blur*2)
-                                        });
-                                        context.save();
-                                        context.translate(options.blur,options.blur);
-                                        context.shadowColor = options.color;
-                                        if ( options.blurMethod == 'native' ) {
-                                            context.shadowBlur = options.blur;
-                                        }
-
-                                        //Make shadow separated from original image
-                                        context.shadowOffsetX = imageWidth+options.blur;
-                                        context.shadowOffsetY = 0;
-
-                                        //Draw image outside canvas, that shadow will appear on original image place
-                                        context.drawImage(img, -imageWidth-options.blur, 0, imageWidth, imageHeight);
-                                        context.restore();
-
-                                        if ( options.blurMethod == 'manual' ) {
-                                            if ( typeof window.stackBoxBlurCanvasRGBA == "function" ) {
-                                                //Dirty hack, to pass canvas into stackBoxBlurCanvasRGBA function
-                                                var origin = document.getElementById;
-                                                document.getElementById = function() { return shadow; }
-                                                stackBoxBlurCanvasRGBA( "canvas", 0, 0, imageWidth+(options.blur*2), imageHeight+(options.blur*2), options.blur, 1 );
-                                                document.getElementById = origin;
-                                            } else {
-                                                throw "StackBoxBlur.js is required for manual blurring. See http://www.quasimondo.com/BoxBlurForCanvas/FastBlur2Demo.html";
-                                            }
-                                        }
-                                    } catch(e) {
-                                        return;
-                                    }
-                                } else {
-                                    //ToDo MSIE Supporting
-                                    return;
-                                }
-                                $(shadow).css({
-                                    display: "block",
-                                    border: 0,
-                                    position: "absolute",
-                                    top: options.offsetY - options.blur,
-                                    left: options.offsetX - options.blur
-                                });
-
-                                var $wrapper = $(/^a|span$/i.test($img.parent().prop("tagName")) ? "<span />" : "<div />"),
-                                    wrapper = $wrapper[0];
-
-                                $wrapper.insertAfter(img).append([img, shadow])
-
-                                wrapper.className = img.className;
-                                $wrapper.addClass(v.wrapperClass);
-
-                                $.data(img, v.dataKey, wrapper.style.cssText = img.style.cssText);
-                                $wrapper.css({
-                                    display: "block",
-                                    width: imageWidth,
-                                    height: imageHeight,
-                                    position: "relative"
-                                });
-                                img.style.cssText = "display: block; border: 0px; position:relative; z-index:2; ";
-                                img.className = v.imgClass;
-                            }
-
-                            if (img.complete)
-                                doShadow();
-                            else
-                                $img.load(doShadow);
-                        },0);
+                        setTimeout($.proxy(function() {
+                            new Context(this, options).init();
+                        },this),100);
                     });
+
                 },this),
                 "show" : $.proxy(function() {
 
@@ -124,25 +143,11 @@
 
                 },this),
                 "unshadow" : $.proxy(function() {
-                    return this.unbind("load").each(function() {
-                        var img = this,
-                            styles = $.data(this, v.dataKey),
-                            wrapper;
-
-                        //ToDo more effecient checking
-                        if (styles !== undefined) {
-                            wrapper = img.parentNode;
-                            img.className = wrapper.className;
-                            img.style.cssText = styles;
-                            $.removeData(img, v.dataKey);
-                            wrapper.parentNode.replaceChild(img, wrapper);
-                        }
-                    });
                 },this)
             };
-            $shadow.init();
+            $multiplexer.init();
 
-            return $shadow;
+            return $multiplexer;
         }
     });
 
